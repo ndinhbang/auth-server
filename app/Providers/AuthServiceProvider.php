40adede305e49b5d52262e942c6a8790c6096449
Nguyen Dinh Bang <bangnd@abilive.vn>
2022-06-17 13:20:42 +0700

    config passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Passport\AuthCode;
+use App\Models\Passport\Client;
+use App\Models\Passport\PersonalAccessClient;
+use App\Models\Passport\RefreshToken;
+use App\Models\Passport\Token;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +30,20 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::hashClientSecrets();
+        if (! $this->app->routesAreCached()) {
+            Passport::routes();
+        }
 
+        Passport::tokensExpireIn(now()->addMinutes(15));
+        Passport::refreshTokensExpireIn(now()->addDays(7));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(3));
+
+        Passport::useTokenModel(Token::class);
+        Passport::useClientModel(Client::class);
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::useRefreshTokenModel(RefreshToken::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
         //
     }
 }
