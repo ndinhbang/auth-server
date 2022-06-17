009e16f6aa0fb37c2a64d3e198e5019681b85339
Nguyen Dinh Bang <bangnd@abilive.vn>
2022-06-17 17:05:37 +0700

    wip

--- app/Models/Passport/Client.php
+++ app/Models/Passport/Client.php
@@ -2,13 +2,10 @@
 
 namespace App\Models\Passport;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Passport\Client as PassportClient;
 
 class Client extends PassportClient
 {
-    use HasFactory;
-
     /**
      * Determine if the client should skip the authorization prompt.
      *
